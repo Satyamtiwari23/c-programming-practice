a2539450781f7d5926cf61013e94a0a2a519239b
Satyamtiwari23 <sttiwari9211@gmail.com>
2026-05-16 17:06:40 +0530

    Quick Sort

---
+++ DSA_C/sorting/quick_sorting.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void traverse_array(int array[] , int n ){
+    printf("Array Elements are: \n");
+    for(int i = 0 ; i < n ; i++){
+        printf("%d  ", array[i]);
+    }
+    printf("\n");
+}
+
+void quick_sort(int array[] , int n){
+    
+}
+int main(){
+    int n ;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0 ; i < n ; i++){
+        printf("Enter %d element of array: ", (i+1));
+        scanf("%d", &arr[i]);
+    }
+
+    // selection Sorting Algorithm
+    quick_sort(arr , n);
+    traverse_array(arr , n);
+}
